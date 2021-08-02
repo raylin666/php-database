2a6c7120d58292eaf5f005ed873c366139503af6
linshan <1099013371@qq.com>
2021-08-02 09:17:20 +0800

    fixed: 非协程下连接池release问题

--- src/DB.php
+++ src/DB.php
@@ -63,6 +63,8 @@ class DB extends Manager
                     Coroutine::defer(function () use ($connectionPool) {
                         $connectionPool->release();
                     });
+                } else {
+                    $connectionPool->release();
                 }
             }
         }
